68a524c10402a5d9cf1a947e7301b160544d6f44
Ndyla_Aprlyla <111104750+NadilaZoroLuffy@users.noreply.github.com>
2023-01-10 14:57:35 +0700

    Add files via upload

---
+++ projek GSPRO/custumers/milk.php
@@ -0,0 +1,54 @@
+<?php
+
+require '../functionUser.php';
+require '../layout/navbar.php';
+
+?>
+
+<div class="container"><br><br>
+      <a href="../index.php" class="btn btn-outline-success px-4 py-2">Kembali</a>
+    <div class="row text-center" style="margin-bottom: 56px">
+      <div class="col">
+          <h1>Breads</h1><br>
+          <p class="labels pt-3 py-5">Semuanya disini Expaird 23 - Desember - 2022</p>
+      </div>
+  </div>
+  <div class="row justify-content-between mb-md-5 mb-3">
+    <div class="col-md-3 mb-3 mb-md-0">
+        <img src="../foto/sariroti.jpg" class="w-100">
+        <h3>Sari Roti</h3>
+        <div class="harga">Rp. 5.000,00</div><br>
+        <ul class="navbar-nav ms-auto me-1 mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a href="../payment/pay.php" class="btn btn-primary px-4 py-2">Beli</a>
+                <a href="login/index.php" class="btn btn-warning px-4 py-2">Keranjang</a>
+              </li>
+        </ul>
+    </div>
+
+    <div class="col-md-3 mb-md-0">
+        <img src="../foto/aoka.jpg" class="w-100">
+        <h3>Aoka</h3>
+        <div class="harga">Rp. 7.000,00</div><br>
+        <ul class="navbar-nav ms-auto me-1 mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a href="../payment/pay.php" class="btn btn-primary px-4 py-2">Beli</a>
+                <a href="login/index.php" class="btn btn-warning px-4 py-2">Keranjang</a>
+              </li>
+        </ul>
+    </div>
+    
+    <div class="col-md-3 mb-md-0">
+        <img src="../foto/kmsan.jpg" class="w-100">
+        <h3>Roti Kemasan</h3>
+        <div class="harga">Rp. 8.000,00</div><br>
+        <ul class="navbar-nav ms-auto me-1 mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a href="../payment/pay.php" class="btn btn-primary px-4 py-2">Beli</a>
+                <a href="login/index.php" class="btn btn-warning px-4 py-2">Keranjang</a>
+              </li>
+        </ul>
+    </div>
+    </div>
+  </div>
+  </div>
---
+++ projek GSPRO/index.php
@@ -0,0 +1,112 @@
+<?php
+
+require 'functionUser.php';
+
+require 'layout/navbar.php';
+
+?>
+
+</nav>
+      <main class="container mt-3">
+        <div class="row d-flex justify-content-center">
+            <div class="col-10">
+                <div class="banner">
+                    <img src="foto/border.jpg" class="img-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center mt-5">
+            <div class="col-12">
+                <h1 class="heading">Categories</h1>
+            </div>
+        </div><br>
+        <div class="row d-flex justify-content-center mt-3">
+          <div class="col-6 col-md-4 col-lg-2 col-xl-2 mb-3">
+              <div class="card card-categories py-3">
+                <div class="img-card text-center">
+                  <img src="foto/snack.png" class="img-fluid">
+                </div>
+                <div class="text-card text-center mt-2 fw-bold">Snack</div>
+              </div><br>
+              <a class="nav-link mx-2" href="custumers/snack.php">Snack</a>
+          </div><br>
+
+          <div class="col-6 col-md-4 col-lg-2 col-xl-2 mb-3">
+            <div class="card card-categories py-3">
+              <div class="img-card text-center">
+                <img src="foto/drink.png" class="img-fluid">
+              </div>
+              <div class="text-card text-center mt-2 fw-bold">Mineral</div>
+            </div><br>
+            <a class="nav-link mx-2" href="custumers/mineral.php">Mineral</a>
+        </div><br>
+
+        <div class="col-6 col-md-4 col-lg-2 col-xl-2 mb-3">
+          <div class="card card-categories py-3">
+            <div class="img-card text-center">
+              <img src="foto/burger.png" class="img-fluid">
+            </div>
+            <div class="text-card text-center mt-2 fw-bold">Food</div>
+          </div><br>
+          <a class="nav-link mx-2" href="custumers/food.php">Food</a>
+      </div><br>
+
+      <div class="col-6 col-md-4 col-lg-2 col-xl-2 mb-3">
+        <div class="card card-categories py-3">
+          <div class="img-card text-center">
+            <img src="foto/milk.png" class="img-fluid">
+          </div>
+          <div class="text-card text-center mt-2 fw-bold">Milk</div>
+        </div><br>
+        <a class="nav-link mx-2" href="custumers/milk.php">Milk</a>
+    </div><br>
+
+      <div class="col-6 col-md-4 col-lg-2 col-xl-2 mb-3">
+        <div class="card card-categories py-3">
+          <div class="img-card text-center">
+            <img src="foto/roti.png" class="img-fluid">
+          </div>
+          <div class="text-card text-center mt-2 fw-bold">Bread</div>
+        </div><br>
+        <a class="nav-link mx-2" href="custumers/bread.php">Bread</a>
+    </div>
+    <div class="row d-flex justify-content-center mt-5">
+      <div class="col-12">
+          <h1 class="heading">Popular Product</h1>
+      </div>
+  </div>
+  <div class="row d-flex justify-content-center mt-3">
+    <div class="col-lg-3 col-xl-3 col-md-6 col-12 mb-5">
+      <div><img src="foto/snak.jpg" class="img-fluid"></div>
+      <div class="mt-2">
+        <h6>Canitos</h6>
+        <div class="harga">Rp. 15.000,00</div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-xl-3 col-md-6 col-12 mb-5">
+      <div><img src="foto/oreo.jpg" class="img-fluid"></div>
+      <div class="mt-2">
+        <h6>Oreo</h6>
+        <div class="harga">Rp. 8.000,00</div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-xl-3 col-md-6 col-12 mb-5">
+      <div><img src="foto/snak.jpg" class="img-fluid"></div>
+      <div class="mt-2">
+        <h6>Roti</h6>
+        <div class="harga">Rp. 13.000,00</div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-xl-3 col-md-6 col-12 mb-5">
+      <div><img src="foto/oreo.jpg" class="img-fluid"></div>
+      <div class="mt-2">
+        <h6>Jus Jeruk </h6>
+        <div class="harga">Rp. 7.000,00</div>
+      </div>
+    </div>
+  </div>
+      </div> 
+      </main>
+      <footer class="my-4 text-center">
+        Copyright 2022 | All Right Reserved | Nadila Aprilla 
+      </footer>
